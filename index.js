7bd05eba7ec837a163d22d9776556e4c5da0d44d
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2017-02-28 15:02:57 +0100

    inital

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+/*
+ * Copyright (c) 2016. Taimos GmbH http://www.taimos.de
+ */
+
+const argv = require('minimist')(process.argv.slice(2));
+const fileName = argv._[0];
+const deployStyle = argv.deploy || false;
+
+const YAML = require('yamljs');
+
+let params = YAML.load(fileName);
+
+let cliString = '';
+
+for (let key in params) {
+  if (params.hasOwnProperty(key)) {
+    let value = params[key];
+    if (Array.isArray(value)) {
+      value = value.join(',');
+    }
+    if (deployStyle) {
+      cliString += `${key}=${value} `;
+    } else {
+      cliString += `ParameterKey=${key},ParameterValue='${value}' `;
+    }
+  }
+}
+
+console.log(cliString);
